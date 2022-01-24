dd47daf790c8e2cbc09b338d0f89a836cfb61e0b
AliciaLan <alicia.lan@isen.yncrea.fr>
2022-01-24 14:35:39 +0100

    Ajout d'un Toast lorsqu'on clique sur un bouton

--- app/src/main/java/fr/isen/lan/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/lan/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,31 @@ package fr.isen.lan.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun clickEntree(v : View) {
+        createToast("Entrée")
+    }
+
+
+    fun clickPlat(v : View) {
+        createToast("Plat")
+    }
+
+
+    fun clickDessert(v : View) {
+        createToast("Dessert")
+    }
+
+
+    fun createToast (txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
