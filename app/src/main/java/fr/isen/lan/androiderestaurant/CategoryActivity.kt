bcd7bf2c5749a9a8adf30d21ef0f9e4d15034d8e
AliciaLan <alicia.lan@isen.yncrea.fr>
2022-01-31 11:13:42 +0100

    Requête POST (Volley) de l'API

--- app/src/main/java/fr/isen/lan/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/lan/androiderestaurant/CategoryActivity.kt
@@ -3,9 +3,14 @@ package fr.isen.lan.androiderestaurant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import fr.isen.lan.androiderestaurant.databinding.ActivityCategoryBinding
 import fr.isen.lan.androiderestaurant.model.DishModel
+import org.json.JSONObject
 
 const val TITLE_DISH = "titleDish"
 
@@ -31,11 +36,31 @@ class CategoryActivity : AppCompatActivity() {
             dishes.add(DishModel(R.drawable.logo, "Item $i", "desc item $i", "$i"))
         }
 
+        postVolley()
+
         binding.categoryList.adapter = DishAdapter(dishes) {
             val intent = Intent(this, DetailsDishActivity::class.java).apply {
                 putExtra(TITLE_DISH, it)
             }
             startActivity(intent)
         }
+    }
+
+    private fun postVolley() {
+        val url = "http://test.api.catering.bluecodegames.com/menu"
+
+        val params = HashMap<String,Number>()
+        params["id_shop"] = 1
+        val jsonObject = JSONObject(params as Map<*, *>)
+
+        val request = JsonObjectRequest(
+            Request.Method.POST,url,jsonObject,
+            {
+                Log.d("API", it.toString())
+            }, {
+                Log.e("API", it.toString())
+            })
+
+        Volley.newRequestQueue(this).add(request)
     }
 }
--- app/src/main/java/fr/isen/lan/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/lan/androiderestaurant/HomeActivity.kt
@@ -3,7 +3,6 @@ package fr.isen.lan.androiderestaurant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import fr.isen.lan.androiderestaurant.databinding.ActivityHomeBinding
 
 
