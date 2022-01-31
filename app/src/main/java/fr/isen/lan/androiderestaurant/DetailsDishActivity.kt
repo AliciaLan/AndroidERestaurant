9142df2998857a2ebf1e7838d53ed5b6e7046c35
AliciaLan <alicia.lan@isen.yncrea.fr>
2022-01-31 16:15:55 +0100

    ajout de picasso pour les images + images de l'API

--- app/src/main/java/fr/isen/lan/androiderestaurant/DetailsDishActivity.kt
+++ app/src/main/java/fr/isen/lan/androiderestaurant/DetailsDishActivity.kt
@@ -2,6 +2,7 @@ package fr.isen.lan.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
 import fr.isen.lan.androiderestaurant.databinding.ActivityDetailsDishBinding
 import fr.isen.lan.androiderestaurant.model.Dish
 
@@ -20,8 +21,17 @@ class DetailsDishActivity : AppCompatActivity() {
 
         val dish : Dish = intent.getSerializableExtra(DISH) as Dish
         binding.dishTitle.text = dish.name_fr
-        //binding.dishDescription.text = dish.description
         binding.dishPrice.text = dish.prices[0].price
-        //binding.dishImage.setImageResource(dish.image)
+
+        if (dish.images[0] != "") {
+            Picasso.get()
+                .load(dish.images[0])
+                .placeholder(R.drawable.logo)
+                .error(R.drawable.logo)
+                .fit()
+                .into(binding.dishImage)
+        } else {
+            binding.dishImage.setImageResource(R.drawable.logo)
+        }
     }
 }
--- app/src/main/java/fr/isen/lan/androiderestaurant/DishAdapter.kt
+++ app/src/main/java/fr/isen/lan/androiderestaurant/DishAdapter.kt
@@ -5,8 +5,10 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import fr.isen.lan.androiderestaurant.databinding.CategoryCellBinding
 import fr.isen.lan.androiderestaurant.model.Dish
+import org.w3c.dom.Text
 
 class DishAdapter(private val dishes: List<Dish>, private val onDishClick : (Dish) -> Unit) : RecyclerView.Adapter<DishAdapter.DishViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DishViewHolder {
@@ -18,8 +20,20 @@ class DishAdapter(private val dishes: List<Dish>, private val onDishClick : (Dis
 
     override fun onBindViewHolder(holder: DishViewHolder, position: Int) {
         val dish = dishes[position]
-        //holder.imageView.setImageResource(R.drawable.logo)
-        holder.textView.text = dish.name_fr
+        holder.name.text = dish.name_fr
+        holder.price.text = dish.prices[0].price
+
+        if (dish.images[0] != "") {
+            Picasso.get()
+                .load(dish.images[0])
+                .placeholder(R.drawable.logo)
+                .error(R.drawable.logo)
+                .fit()
+                .into(holder.image)
+        } else {
+            holder.image.setImageResource(R.drawable.logo)
+        }
+
         holder.itemView.setOnClickListener {
             onDishClick(dish)
         }
@@ -30,7 +44,8 @@ class DishAdapter(private val dishes: List<Dish>, private val onDishClick : (Dis
     }
 
     class DishViewHolder(binding : CategoryCellBinding) : RecyclerView.ViewHolder(binding.root) {
-       // val imageView: ImageView = binding.listImage
-        val textView: TextView = binding.listTitle
+        val name: TextView = binding.listTitle
+        val price: TextView = binding.listPrice
+        val image: ImageView = binding.listImage
     }
 }
