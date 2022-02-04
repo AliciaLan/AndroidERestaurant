6868b9a4e0985c2875a0cf61e8f356827eff4338
AliciaLan <alicia.lan@isen.yncrea.fr>
2022-02-04 17:27:15 +0100

    Connexion fonctionnelle

--- app/src/main/java/fr/isen/lan/androiderestaurant/LoginFragment.kt
+++ app/src/main/java/fr/isen/lan/androiderestaurant/LoginFragment.kt
@@ -1,11 +1,19 @@
 package fr.isen.lan.androiderestaurant
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import fr.isen.lan.androiderestaurant.databinding.FragmentLoginBinding
+import fr.isen.lan.androiderestaurant.model.LoginData
+import org.json.JSONObject
 
 class LoginFragment : Fragment() {
     private lateinit var binding : FragmentLoginBinding
@@ -25,7 +33,35 @@ class LoginFragment : Fragment() {
         }
 
         binding.loginButton.setOnClickListener {
+            val loginData = LoginData(
+                binding.loginEmailEditText.text.toString(),
+                binding.loginPasswordEditText.text.toString()
+            )
 
+            login(loginData)
         }
+    }
+
+    private fun login(loginData : LoginData) {
+        val url = "http://test.api.catering.bluecodegames.com/user/login"
+
+        val params = HashMap<String, String>()
+        params["email"] = loginData.email
+        params["password"] = loginData.password
+        val jsonObject = JSONObject(params as Map<*, *>)
+
+        val request = JsonObjectRequest(
+            Request.Method.POST, url, jsonObject,
+            {
+                Log.d("response", it.toString())
+            }, {
+                Log.e("API", it.toString())
+                Toast.makeText(context, "API request failed", Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        request.retryPolicy = DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, 0, 1f)
+
+        Volley.newRequestQueue(context).add(request)
     }
 }
---
+++ app/src/main/java/fr/isen/lan/androiderestaurant/model/LoginData.kt
@@ -0,0 +1,6 @@
+package fr.isen.lan.androiderestaurant.model
+
+data class LoginData(
+    val email : String,
+    val password : String
+)
